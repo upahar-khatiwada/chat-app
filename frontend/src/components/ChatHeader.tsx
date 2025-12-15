e22024dbb8217575f6102b87674649aa276a8b73
upahar windows <upaharlol@gmail.com>
2025-12-15 16:34:50 +0545

    feat: add reactjs-popup for user profile display in ChatHeader component

--- frontend/src/components/ChatHeader.tsx
+++ frontend/src/components/ChatHeader.tsx
@@ -2,6 +2,7 @@ import { CiVideoOn } from "react-icons/ci";
 import { IoIosCall } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
 import type User from "../interfaces/user_interface";
+import Popup from "reactjs-popup";
 
 interface ChatHeaderProps {
   userChattingWith: User;
@@ -14,14 +15,24 @@ const ChatHeader = ({
 }: ChatHeaderProps) => {
   return (
     <nav className="flex justify-between items-center mx-2 mt-2 mb-2">
-      <div className="flex gap-2 items-center cursor-pointer hover:bg-[#9b7d6d] hover:rounded-2xl transition-all duration-300 p-1">
-        <img
-          src={userChattingWith.avatar}
-          className="w-10 h-10 rounded-full"
-          referrerPolicy="no-referrer"
-        />
-        <span className="font-semibold">{userChattingWith.fullName}</span>
-      </div>
+      <Popup
+        trigger={
+          <div className="flex gap-2 items-center cursor-pointer hover:bg-[#9b7d6d] hover:rounded-2xl transition-all duration-300 p-1">
+            <img
+              src={userChattingWith.avatar}
+              className="w-10 h-10 rounded-full"
+              referrerPolicy="no-referrer"
+            />
+            <span className="font-semibold">{userChattingWith.fullName}</span>
+          </div>
+        }
+      >
+        <div className="p-4 bg-[#c3b091] rounded shadow-lg">
+          <h2 className="font-bold">Profile</h2>
+          <p>{userChattingWith.fullName}</p>
+          <p>{userChattingWith.email}</p>
+        </div>
+      </Popup>
 
       <div className="flex gap-2">
         <IoIosCall
